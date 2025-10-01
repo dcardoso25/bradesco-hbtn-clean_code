24ac6026dcd7c0dce75682af9022a4df02b25c11
Diego Cardoso <98044200+diego-sbtlab@users.noreply.github.com>
2025-09-30 22:37:18 -0300

    Update Functions.java

--- 0x00/function/Functions.java
+++ 0x00/function/Functions.java
@@ -21,11 +21,13 @@ public class Functions {
 
     //  No side effects
     //B
+    //  No side effects
+    //A
     public User saveUser(User user){
+        if(user.isAdmin){
+            user.setRole(user);
+        }
         return userRepository.save(user);
-    }
-    public void setRole(User user){
-        user.setRole(user);
     }
 
     // Evite duplicados, extraindo para novos métodos
